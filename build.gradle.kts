d959ce42150b71f4371c011c41c70c885441ffd3
Om Gupta <ogupta4242@gmail.com>
2024-06-17 08:57:46 -0700

    Added gauth library

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,11 +14,13 @@ dependencies {
     val junitV = "5.10.2"
     val miglayoutV = "11.3"
     val mdlafV = "1.1.4"
+    val gauthV = "1.23.0"
     testImplementation(platform("org.junit:junit-bom:$junitV"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     compileOnly("org.projectlombok:lombok:$lombokV")
     implementation("io.github.vincenzopalazzo:material-ui-swing:$mdlafV")
     implementation("com.miglayout:miglayout-swing:$miglayoutV")
+    implementation("com.google.auth:google-auth-library-oauth2-http:$gauthV")
 }
 
 tasks.test {
